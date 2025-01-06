10cd5c214c224d647072b96890a17745bb81f928
Arush <55015285+aruahaha@users.noreply.github.com>
2025-01-06 17:31:59 +0530

    nodemon installed

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const app = express();
 const port = process.env.PORT || 3001;
-
+dotenv.config();
 app.use(cors());
 app.use(express.json());
 
-app.listen(process.env.PORT, () => {
+app.get("/", (req, res) => {
+  res.send(`Server started at http://localhost:${port}`);
+});
+
+app.listen(port, () => {
   console.log("Server started at", port);
 });
