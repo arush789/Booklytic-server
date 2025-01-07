d7be0b31055f8b37ceab9b481704678fcab74ec9
Arush <55015285+aruahaha@users.noreply.github.com>
2025-01-07 22:22:08 +0530

    postgres sql local added

--- index.js
+++ index.js
@@ -7,8 +7,38 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send(`Server started at http://localhost:${port}`);
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+app.get("/test", async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query(
+      "SELECT brand FROM cars WHERE brand = 'Ford'"
+    );
+    client.release();
+    console.log("Data:", result.rows);
+
+    res.json(result.rows);
+  } catch (error) {
+    console.error("Database error:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.get("/add-data", async (req, res) => {
+  const client = await pool.connect();
+  await client.query(
+    "INSERT INTO cars (brand, model, year) VALUES ('BMW', 'M8', '1999')"
+  );
+  client.release();
 });
 
 app.listen(port, () => {
